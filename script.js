3d375094e700289c081f7e24c3f3e67f7c473276
Anushri <112797477+anushri0802@users.noreply.github.com>
2025-02-27 23:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtn = document.getElementById("closeModal");
+
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "block"; 
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none"; 
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none"; 
+  }
+});
